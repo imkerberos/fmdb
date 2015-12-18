4ffbbf2fd63601bc65ba06c8b32680c89514de80
Zhang Qigang <zhangqigang@letv.com>
2015-12-18 17:11:27 +0800

    support carthage framework

---
+++ FMDB/FMDB.h
@@ -0,0 +1,24 @@
+//
+//  FMDB.h
+//  FMDB
+//
+//  Created by Kerberos Zhang on 15/12/18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FMDB.
+FOUNDATION_EXPORT double FMDBVersionNumber;
+
+//! Project version string for FMDB.
+FOUNDATION_EXPORT const unsigned char FMDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMDB/PublicHeader.h>
+
+
+#import <FMDB/FMDatabase.h>
+#import <FMDB/FMResultSet.h>
+#import <FMDB/FMDatabaseQueue.h>
+#import <FMDB/FMDatabaseAdditions.h>
+#import <FMDB/FMDatabasePool.h>

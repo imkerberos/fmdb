cf36c6b08c1b7160985c1ee61c62c37119c0f320
Zhang Qigang <zhangqigang@letv.com>
2015-12-18 19:03:05 +0800

    add carthage support.

--- FMDB/FMDB.h
+++ FMDB/FMDB.h
@@ -16,9 +16,8 @@ FOUNDATION_EXPORT const unsigned char FMDBVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FMDB/PublicHeader.h>
 
-
 #import <FMDB/FMDatabase.h>
 #import <FMDB/FMResultSet.h>
-#import <FMDB/FMDatabaseQueue.h>
 #import <FMDB/FMDatabaseAdditions.h>
+#import <FMDB/FMDatabaseQueue.h>
 #import <FMDB/FMDatabasePool.h>
